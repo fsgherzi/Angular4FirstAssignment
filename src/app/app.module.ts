1ea527414a4904842e9fbd0dbf487bc72679a4ec
Francesco Sgherzi <francesco.sgherzi@gmail.com>
2017-08-26 12:59:19 +0200

    Lecture 2: Assignement 1 -> First point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './Components/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './Components/success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
